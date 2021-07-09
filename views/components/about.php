6d6e5e902bc64068a6481ec962431995570e667d
mechacktambwe <mechacktambwe@gmail.com>
2021-07-09 13:34:43 +0200

    feat: add some images

---
+++ views/components/about.php
@@ -0,0 +1,9 @@
+<section class="about bg-primary text-light">
+    <div class="container">
+    <div class="d-flex  justify-content-center" >
+            <h2 class="section-title title text-center border-color-white">
+                A PROPOS
+            </h2>   
+        </div>
+    </div>
+</section>
---
+++ views/components/programme.php
@@ -0,0 +1,41 @@
+<section class="programme">
+    <div class="container ">
+        <div class="d-flex  justify-content-center" >
+            <h2 class="section-title title text-center">
+                VISAS
+            </h2>   
+        </div>
+        <div class="programme-list p-5">
+            <div class="row">
+                <div class="card visa-card" style="width: 15rem;">
+                        <img src="../../assets/images/touriste.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">visa Touristique</h5>
+                            <a href="#" class="btn btn-primary text-center">Faire une demande</a>
+                        </div>
+                </div>
+                <div class="card visa-card" style="width: 15rem;">
+                        <img src="../../assets/images/work.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">visa Travail</h5>
+                            <a href="#" class="btn btn-primary text-center">Faire une demande</a>
+                        </div>
+                </div> 
+                <div class="card visa-card" style="width: 15rem;">
+                        <img src="../../assets/images/etudes.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">visa Etude</h5>
+                            <a href="#" class="btn btn-primary text-center">Faire une demande</a>
+                        </div>
+                </div> 
+                <div class="card visa-card" style="width: 15rem;">
+                        <img src="../../assets/images/travel.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">immigration</h5>
+                            <a href="#" class="btn btn-primary text-center">Faire une demande</a>
+                        </div>
+                </div>    
+            </div>
+        </div>
+    </div>
+</section>
